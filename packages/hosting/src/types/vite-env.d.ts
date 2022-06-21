6b7f429a61b51adb098855eceafb0ddef0b92b9a
oriverk <oriveryk@gmail.com>
2022-06-22 00:02:11 +0900

    hosting: modify .env.local and vite-env.d.ts

--- packages/hosting/src/types/vite-env.d.ts
+++ packages/hosting/src/types/vite-env.d.ts
@@ -1,1 +1,10 @@
+/* eslint-disable no-unused-vars */
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_CLOUDINARY_NAME: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
