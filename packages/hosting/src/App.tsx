4271b4becdb8bb7fc33feb6c63de12e1d8333379
oriverk <oriveryk@gmail.com>
2022-06-22 00:03:17 +0900

    hosting: remove unused styled css

--- packages/hosting/src/App.tsx
+++ packages/hosting/src/App.tsx
@@ -4,7 +4,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Layout } from "./components/Layout";
 import { Home } from "./pages/home"
 import { PageNotFound } from "./pages/404";
-import "./styles/tailwind.css";
 
 const App: FC = () => (
   <BrowserRouter>
--- packages/hosting/src/main.tsx
+++ packages/hosting/src/main.tsx
@@ -1,8 +1,7 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./styles/index.css";
-import "./styles/tailwind.css"
+import "./styles/tailwind.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <StrictMode>
