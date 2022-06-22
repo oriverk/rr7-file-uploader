991f94ade2981e5e9ab7fbc8475c6bd4d2402528
oriverk <oriveryk@gmail.com>
2022-06-23 01:17:29 +0900

    hosting: setup firebase

--- packages/hosting/src/types/vite-env.d.ts
+++ packages/hosting/src/types/vite-env.d.ts
@@ -2,7 +2,19 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
+  // cloudinary
   readonly VITE_CLOUDINARY_NAME: string;
+  // firebase
+  readonly VITE_FIREBASE_APP_KEY: string;
+  readonly VITE_FIREBASE_AUTH_DOMAIN: string;
+  readonly VITE_FIREBASE_DATABASE_URL: string;
+  readonly VITE_FIREBASE_PROJECT_ID: string;
+  readonly VITE_FIREBASE_STORAGEBUCKET: string;
+  readonly VITE_FIREBASE_MESSAGING_SENDER_ID: string;
+  readonly VITE_FIREBASE_APP_ID: string;
+  readonly VITE_FIREBASE_MEASUREMENT_ID: string;
+
+  readonly VITE_FIREBASE_AUTH_PERSISIT: boolean;
 }
 
 interface ImportMeta {
