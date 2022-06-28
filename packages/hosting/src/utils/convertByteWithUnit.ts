018feb8fd7c6f036aa4ae47f8c85639e6d2f6f64
oriverk <oriveryk@gmail.com>
2022-06-28 22:59:59 +0900

    hosting: add utils to parser file info

---
+++ packages/hosting/src/utils/convertByteWithUnit.ts
@@ -0,0 +1,34 @@
+/**
+ * 入力されたファイルサイズを元に計算につかうバイト数と単位を返却
+ * @param size
+ * @return
+ */
+function getTarget (size: number) {
+  const kb = 1024
+  const mb = kb**2
+  const gb = kb**3
+  const tb = kb**4
+
+  if (size >= tb) return { target: tb, unit: 'TB' }
+  if (size >= gb) return { target: gb, unit: 'GB' }
+  if (size >= mb) return { target: mb, unit: 'MB' }
+  if (size >= kb) return { target: kb, unit: 'KB' }
+
+  return { target: null, unit: 'byte' }
+}
+
+/**
+ * ファイルサイズを適切な単位に変換(小数点第二位まで求める)
+ * @param size
+ * @param decimal 小数点以下第n位まで求めるか
+ * @return
+ */
+export function convertByteWithUnit(size: number, decimal = 2) {
+  const { target, unit } = getTarget(size)
+  const d = 10**decimal
+
+  const newSize = target !== null ? Math.floor((size / target) * d) / d : size
+
+  return newSize + unit
+}
+
---
+++ packages/hosting/src/utils/dateString.ts
@@ -0,0 +1,8 @@
+import { format } from "date-fns"
+
+/**
+ * format Date to yyyy-MM-dd HH:mm
+ * @param date
+ * @returns
+ */
+export const dateString = (date: Date) => format(date, 'yyyy-MM-dd HH:mm')
