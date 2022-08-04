7aca797fd4d8ad2f32c696853cf9e97a5c5aa9ae
oriverk <oriveryk@gmail.com>
2022-08-05 01:17:41 +0900

    modify tailwind.config.js

--- packages/hosting/tailwind.config.js
+++ packages/hosting/tailwind.config.js
@@ -1,6 +1,7 @@
 /* eslint-disable global-require */
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class',
   content: [
     "./index.html",
     "./src/**/*.{jsx,tsx}",
