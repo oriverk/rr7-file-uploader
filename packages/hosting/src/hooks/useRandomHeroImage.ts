466b34c7d262484133daca56de7564fb0ea18f53
oriverk <oriveryk@gmail.com>
2022-11-24 15:17:00 +0900

    split otono json data from useRandomHeroImage to src/constants

--- packages/hosting/src/hooks/useRandomHeroImage.ts
+++ packages/hosting/src/hooks/useRandomHeroImage.ts
@@ -1,54 +1,6 @@
 import { useState, useEffect } from "react";
 import { getCloudinaryImage } from "@/utils/getCloudinaryImage";
-
-const Data: Record<string, string[]> = {
-  // chapter22: 天廻争乱～覇者開闢に抗いし戦鬼～
-  22: [
-    "uesugi",
-    "mouri",
-    "mogami",
-    "honda",
-    "sanada",
-    "fukushima",
-    "toyotomi",
-    "toudou",
-    "chosokabe",
-    "kobayakawa",
-    "kuroda",
-    "shouneiou",
-  ],
-  // chapter23: 下天開闢～導かれし宿星～
-  23: [
-    "saito_dousan",
-    "imagawa_yoshimoto",
-    "akechi_mitsuhide",
-    "takeda_harunobu",
-    "nagao_kagetora",
-    "matsudaira_motoyasu",
-    "hashiba_hideyoshi",
-    "matsunaga_hisahide",
-    "ootomo_sourin",
-    "mori_ranmaru",
-    "kuki_yoshitaka",
-    "araki_murashige",
-    "oda_nobunaga",
-  ],
-  24: [
-    "hachisuka",
-    "hori",
-    "kato",
-    "kunohe",
-    "kuroda",
-    "maeda",
-    "saika",
-    "sanada",
-    "shibata",
-    "shimazu",
-    "takenaka",
-    "toyotomi",
-    "ukita",
-  ],
-};
+import { Ootono } from "@/constants/ootono";
 
 type ReturnType = {
   name: string;
@@ -66,7 +18,7 @@ export function useRandomHeroImage(chapter: number) {
   });
 
   useEffect(() => {
-    const images = Data[chapter];
+    const images = Ootono[chapter];
     const random = Math.floor(Math.random() * images.length);
     const name = images[random];
     const ext = chapter < 24 ? "webp" : "jpg";
